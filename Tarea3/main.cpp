45b51d0ebdb682eae3f08d08847937ff8f366f2f
CristianAlfaro <00167917@uca.edu.sv>
2020-04-24 16:45:10 -0600

    pequeños cambios

--- Tarea3/main.cpp
+++ Tarea3/main.cpp
@@ -14,8 +14,8 @@ int main(){
     Vector b;
     Vector T;
 
-    cout << "IMPLEMENTACI"<<char(224)<<"N DEL M"<<char(144)<<"TODO DE LOS ELEMENTOS FINITOS\n"
-         << "\t- TRANSFERENCIA DE CALOR\n" << "\t- 1 DIMENSI"<<char(224)<<"N\n"
+    cout << "IMPLEMENTACION DEL METODO DE LOS ELEMENTOS FINITOS\n"
+         << "\t- TRANSFERENCIA DE CALOR\n" << "\t- 1 DIMENSION\n"
          << "\t- FUNCIONES DE FORMA LINEALES\n" << "\t- PESOS DE GALERKIN\n"
          << "*********************************************************************************\n\n";
 
